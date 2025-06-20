58917a814ae979c2399276b17149a71e380d2bae
Vaibhav Shukla <shuklavaibhav267@gmail.com>
2025-06-20 16:21:10 +0530

    Delete src/Components/utils directory

--- src/Components/utils/FoodAPI.js
+++
@@ -1,4 +0,0 @@
-export const REST_LIST_URI="https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=26.449923&lng=80.3318736&carousel=true&third_party_vendor=1";
-
-export const REST_DETAIL_URI="https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.449923&lng=80.3318736&restaurantId=";
-
--- src/Components/utils/FoodImg.js
+++
@@ -1,7 +0,0 @@
-export const FoodImage="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/"
-
-
-export const AppLogo="https://cdn-icons-png.flaticon.com/128/1532/1532688.png"
-
-
-export const SearchLogo="https://cdn-icons-png.flaticon.com/128/13755/13755740.png"
